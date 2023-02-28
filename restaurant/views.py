a281d5d8a28da953d110240459ccaaacc5203a10
Kevin Peng <fatexrebirth@gmail.com>
2023-02-28 09:46:07 +0800

    Securing the table booking API

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
@@ -15,3 +16,4 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
