975674decd78281a9663e3179feda7005d5a32ea
Kevin Peng <fatexrebirth@gmail.com>
2023-03-01 10:31:25 +0800

    Final adjustments

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+http://localhost:8000/auth/users/
+http://localhost:8000/auth/token/login
+http://localhost:8000/auth/token/logout
+http://localhost:8000/restaurant
+http://localhost:8000/restaurant/menu/
+http://localhost:8000/restaurant/menu/:id
+http://localhost:8000/restaurant/booking/tables/
+http://localhost:8000/restaurant/booking/tables/:id
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
 from .models import Menu, Booking
@@ -5,6 +6,9 @@ from .serializers import MenuSerializer, BookingSerializer
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
